e4894a40c83df0e86269d3329e661e134f3efaea
pvelazquez1096 <86578496+pvelazquez1096@users.noreply.github.com>
2021-12-18 14:46:50 -0600

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./topbar.css"
+export default function Topbar() {
+    return (
+        <div className='topbar'>
+            <div className="topbarWrapper">
+                <div className="topleft">
+                    left
+                </div>
+                <div className="topright">
+                    right
+                </div>
+            </div>
+        </div>
+    )
+}
