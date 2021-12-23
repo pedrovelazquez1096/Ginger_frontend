a6f45f1c98d7eb73739fa9b4841b44797d42cd57
pvelazquez1096 <86578496+pvelazquez1096@users.noreply.github.com>
2021-12-23 16:04:28 -0600

    Updates

---
+++ precache-manifest.3a627820dc9e61a2690681ec7e88298c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5474e1199f97f172ea2f2dc43c7f6ebc",
+    "url": "/Ginger_frontend/index.html"
+  },
+  {
+    "revision": "7a53caf862980f69cfc8",
+    "url": "/Ginger_frontend/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9b6366506ae8b66f8460",
+    "url": "/Ginger_frontend/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "7a53caf862980f69cfc8",
+    "url": "/Ginger_frontend/static/js/2.6b93418c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Ginger_frontend/static/js/2.6b93418c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b6366506ae8b66f8460",
+    "url": "/Ginger_frontend/static/js/main.d4b9b731.chunk.js"
+  },
+  {
+    "revision": "7489fc4525dd2f6b3c80",
+    "url": "/Ginger_frontend/static/js/runtime-main.247bf85b.js"
+  }
+]);
